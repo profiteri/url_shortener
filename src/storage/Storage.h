c9736b5ef49ec93d5783df4b3dd3d58b6fa8419f
Valery <valery.tsarou@tum.de>
2024-02-05 18:53:53 +0000

    put url operations in storage class
    Former-commit-id: 5be0b18debb928ea6e97674a097b98e375abee10

---
+++ src/storage/Storage.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <functional>
+#include <fstream>
+
+class Storage {
+
+    const std::string longToShortFileName = "/space/shortToLong.txt";
+    const std::string shortToLongFileName = "/space/longToShort.txt";
+
+    std::ifstream shortToLongFile;
+    std::ifstream longToShortFile;
+
+    std::unordered_map<std::string, std::string> shortToLong;
+    std::unordered_map<std::string, std::string> longToShort;    
+
+    std::string toBase62(size_t hashValue);
+
+public:
+
+    Storage();
+    ~Storage();
+
+    std::string getLongUrl(const std::string& shortUrl);
+    std::string generateShortUrl(const std::string& longUrl);
+
+};
